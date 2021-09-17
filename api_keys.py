19c51f661a0d4b983cf8b4040c996f17c56ea202
Josepht7979 <66784996+Josepht7979@users.noreply.github.com>
2021-09-17 18:19:25 +1000

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,7 +1,7 @@
 # OpenWeatherMap API Key
-weather_api_key = "55fb9823a20c70e9d28d096302f04a28"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyBArAwcHkPsT3oviH-M9A1sqQArefglt1I"
+g_key = ""
 
 
