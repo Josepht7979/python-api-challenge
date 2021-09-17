d5da87f9f8574d06aeb98b7d14a8ba7a7712591f
Josepht7979 <josepht7979@gmail.com>
2021-09-17 18:17:18 +1000

    final revision

---
+++ api_keys.py
@@ -0,0 +1,7 @@
+# OpenWeatherMap API Key
+weather_api_key = "55fb9823a20c70e9d28d096302f04a28"
+
+# Google API Key
+g_key = "AIzaSyBArAwcHkPsT3oviH-M9A1sqQArefglt1I"
+
+
---
+++ starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "55fb9823a20c70e9d28d096302f04a28"
+
+# Google API Key
+g_key = "AIzaSyCHG7_AH1S3pM9J7hI_AYLmcveEO8rPMTU"
